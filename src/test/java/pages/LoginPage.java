c6361b9603ecd76e4e4e10647f2cbf434b2d44e1
Venkatesham Dumpeti <Venkatesham.dumpeti@gmail.com>
2021-04-13 22:39:13 -0400

    LoginPageTests Fixed

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -15,62 +15,15 @@ public class LoginPage {
 	
 	}
 	
-	@FindBy(xpath = "//title[text()=\"Login - My Store\"]")
+    public String getLoginPageTitle() {
+    	String PageTitle = driver.getTitle();
+        return PageTitle;
 	
-	WebElement LoginPageTitle;
+	}
 	
 	@FindBy(xpath = "//h3[text()=\"Create an account\"]")
 	
 	WebElement CreateAnAccount;
-	
-	@FindBy(xpath = "//input[@id=\"email_create\"]")
-	
-	WebElement inputEmailForAccountCreate;
-	
-	@FindBy(xpath = "//button[@id=\"SubmitCreate\"]")
-	
-	WebElement CreateAccountSubmitButton;
-	
-	@FindBy(xpath = "//h3[text()=\"Already registered?\"]")
-	
-	WebElement AlreadyRegister;
-	
-	@FindBy(id="email")
-	
-	WebElement inputEmailForLogin;
-	
-	@FindBy(id="passwd")
-	
-	WebElement inputPwdForLogin;
-	
-	@FindBy(id="SubmitLogin")
-	
-	WebElement SignIn;
-	
-	@FindBy(xpath = "//a[text()=\"Forgot your password?\"]")
-	
-	WebElement ForgotPassword;
-	
-	@FindBy(xpath = "(//input[@name=\"email\"])[1]")
-	
-	WebElement inputEmailforForgotPassword;
-	
-	@FindBy(xpath = "//span[text()=\"Retrieve Password\"]")
-	
-	WebElement RetrievePassword;
-	
-	@FindBy(xpath = "//p[text()=\"A confirmation email has been sent to your address: test.Testing@gmail.com\"]")
-	
-	WebElement ForgotPasswordSuccessmessage;
-	
-	
-	
-	public String getLoginPageTitle() {
-		
-		String PageTitle = LoginPageTitle.getText();
-		return PageTitle;
-			
-	}
 	
 	public String getCreateAnAccountHeadingText() {
 		
@@ -78,10 +31,18 @@ public class LoginPage {
 		return AccountCreationText;
 	}
 	
+	@FindBy(xpath = "//input[@id=\"email_create\"]")
+	
+	WebElement inputEmailForAccountCreate;
+	
 	public void setEmailForCreateAnAccount(String email) {
 		
 		inputEmailForAccountCreate.sendKeys(email);
 	}
+	
+	@FindBy(xpath = "//button[@id=\"SubmitCreate\"]")
+	
+	WebElement CreateAccountSubmitButton;
 	
 	public void clickCreateAnAccountSubmitButton() {
 		
@@ -89,6 +50,10 @@ public class LoginPage {
 	}
 	
 	
+	@FindBy(xpath = "//h3[text()=\"Already registered?\"]")
+	
+	WebElement AlreadyRegister;
+
 	public String getPagesubHeadingAlreadyRegistered() {
 		
 		String title = AlreadyRegister.getText();
@@ -96,16 +61,30 @@ public class LoginPage {
 		
 	}
 	
+	@FindBy(id="email")
+	
+	WebElement inputEmailForLogin;
+	
 	public void setEmailForLogin(String email) {
 		
 	 inputEmailForLogin.sendKeys(email);
 	}
 	
+	@FindBy(id="passwd")
+	
+	WebElement inputPwdForLogin;
+	
+	
 	public void setPwdForLogin(String Pwd) {
+		
+		inputPwdForLogin.sendKeys(Pwd);
+		
+		}
+		
 	
-	inputPwdForLogin.sendKeys(Pwd);
+	@FindBy(id="SubmitLogin")
 	
-	}
+	WebElement SignIn;
 	
 	public void clickSignIn() {
 		
@@ -113,29 +92,46 @@ public class LoginPage {
 		
 	}
 	
+	@FindBy(xpath = "//a[text()=\"Forgot your password?\"]")
+	
+	WebElement ForgotPassword;
+	
 	public void clickForgotPasswordLink() {
 		
 		ForgotPassword.click();
 	}
+	
+	@FindBy(xpath = "(//input[@name=\"email\"])[1]")
+	
+	WebElement inputEmailforForgotPassword;
 	
 	public void setEmailForForgotPassword(String email1) {
 		
 		inputEmailforForgotPassword.sendKeys(email1);
 	}
 	
+	@FindBy(xpath = "//span[text()=\"Retrieve Password\"]")
+	
+	WebElement RetrievePassword;
+	
 	public void clickRetrievePassword() {
 		
 		RetrievePassword.click();
 	}
 	
+	@FindBy(xpath = "//p[text()=\"A confirmation email has been sent to your address: test.Testing@gmail.com\"]")
+	
+	WebElement ForgotPasswordSuccessmessage;
+
 	public String getSuccessMessageforForgotPassword() {
-		
+			
 		String text = ForgotPasswordSuccessmessage.getText();
 		return text;
+			
+		}
 		
-	}
-	
-	
+		
+		
 	
 	
 }
